12fe721cb1bd82eda92b0eb5fe65bde4a5613c93
AxelTelias <axeltelias2003@gmail.com>
2022-10-18 12:07:27 -0300

    first commit

---
+++ www/js/my-app.js
@@ -0,0 +1,65 @@
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+      swipe: 'left',
+    },
+    // Add default routes
+    routes: [
+      {
+        path: '/about/',
+        url: 'about.html',
+      },
+    ]
+    // ... other parameters
+  });
+
+var mainView = app.views.create('.view-main');
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+// Option 1. Using one 'page:init' handler for all pages
+$$(document).on('page:init', function (e) {
+    // Do something here when page loaded and initialized
+    console.log(e);
+})
+
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
+    alert('Hello');
+})
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBJeO2chBKAw1LrP_G9OVMTU5FMaFuebFg",
+  authDomain: "where-do-i-read-it.firebaseapp.com",
+  projectId: "where-do-i-read-it",
+  storageBucket: "where-do-i-read-it.appspot.com",
+  messagingSenderId: "1035047346294",
+  appId: "1:1035047346294:web:c7a3c7f4ce7b8670b6bf57",
+  measurementId: "G-31FMZEXWBP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
